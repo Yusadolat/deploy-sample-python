c6a6725f656b5831042c1ee5ad942d5ff542a06e
Yusadolat <yusadolat@gmail.com>
2022-11-19 12:07:34 +0100

    refactoring

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, request
+import os
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def homepage():
-    if request.method == "GET":
-        return jsonify({"message": "Hello GDG Ilorin!"})
 
-PORT = int(os.environ.get("PORT", 8080))
-if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=PORT)
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# [END run_helloworld_service]
+# [END cloudrun_helloworld_service]
